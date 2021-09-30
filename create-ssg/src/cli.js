4e790388b96d49a3300dbbc5e99bcdf21b7fb38e
Suhhee Kim <suhhee@gmail.com>
2021-09-30 08:38:36 -0400

    commit to push

--- create-ssg/src/cli.js
+++ create-ssg/src/cli.js
@@ -14,12 +14,7 @@ function markDowntoHTML(text) {
         .replace(/\*\*(.*)\*\*/gim, '<b>$1</b><br/>')
         .replace(/\_\_(.*)\_\_/gim, '<b>$1</b><br/>')
         .replace(/\*(.*)\*/gim, '<i>$1</i><br/>')
-
-
-        .replace(/^--- (.*$)/gim, '<hr/>')//horizontal rule
         .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a><br/>")
-        .replace(/\`(.*)\`/gim, '<code>$1</code><br/>') // inline code block
-
         .replace(/\n$/gim, '<br />')
         .replace(/\n/gim,'<p>$1</p>')
         return htmlText.trim();
