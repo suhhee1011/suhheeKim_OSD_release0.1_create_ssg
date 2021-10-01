5a735a585c0e48ff1f0425cc9a9c026de9ce5441
Suhhee Kim <suhhee@gmail.com>
2021-10-01 15:19:52 -0400

    commit to add support convert inline code block from Markdown to HTML

--- create-ssg/src/cli.js
+++ create-ssg/src/cli.js
@@ -13,6 +13,7 @@ function markDowntoHTML(text) {
         .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
         .replace(/\*\*(.*)\*\*/gim, '<b>$1</b><br/>')
         .replace(/\_\_(.*)\_\_/gim, '<b>$1</b><br/>')
+        .replace(/\`(.*)\`/gim, '<code>$1</code><br/>') // inline code block
         .replace(/\*(.*)\*/gim, '<i>$1</i><br/>')
         .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a><br/>")
         .replace(/\n$/gim, '<br />')
