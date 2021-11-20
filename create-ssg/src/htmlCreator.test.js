16e304121bf23d496cb84954b89f05fed471051e
jiyun99 <rrwldbs@gmail.com>
2021-11-20 11:45:08 -0500

    updated h5 h6

--- create-ssg/src/htmlCreator.test.js
+++ create-ssg/src/htmlCreator.test.js
@@ -1,11 +1,17 @@
 const console = require("./htmlCreator.js");
-test("Markdown Grammar translated",() =>{
-    expect(markDowntoHTML("#### test").toBe("<h3>test</h3>"));
+test("Markdown Grammar translated", () => {
+   expect(markDowntoHTML("#### test").toBe("<h3>test</h3>"));
 });
 
-test("HTMLForm changed",() =>{
-    expect(HTMLForm("title", "sample","<p>This is for test</p>","`<meta name='title' content = '[##_title_##]'>").toBe(
-        `<!doctype html>
+test("HTMLForm changed", () => {
+   expect(
+      HTMLForm(
+         "title",
+         "sample",
+         "<p>This is for test</p>",
+         "`<meta name='title' content = '[##_title_##]'>",
+      ).toBe(
+         `<!doctype html>
        <html lang="en">
        <head>
          <meta charset="utf-8">
@@ -17,21 +23,26 @@ test("HTMLForm changed",() =>{
        <p>This is for test</p>
        </body>
        </html>
-       `
-
-    ));
-
+       `,
+      ),
+   );
 });
 
-test("Markdown Grammar changed H4",() =>{
-    expect(markDowntoHTML("#### test").toBe("<h4>test</h4>"));
+test("Markdown Grammar changed H6", () => {
+   expect(markDowntoHTML("###### test").toBe("<h6>test</h6>"));
 });
-test("Markdown Grammar changed H3",() =>{
-    expect(markDowntoHTML("### test").toBe("<h3>test</h3>"));
+test("Markdown Grammar changed H5", () => {
+   expect(markDowntoHTML("##### test").toBe("<h5>test</h5>"));
 });
-test("Markdown Grammar changed H2",() =>{
-    expect(markDowntoHTML("## test").toBe("<h2>test</h2>"));
+test("Markdown Grammar changed H4", () => {
+   expect(markDowntoHTML("#### test").toBe("<h4>test</h4>"));
 });
-test("Markdown Grammar changed H1",() =>{
-    expect(markDowntoHTML("# test").toBe("<h1>test</h1>"));
+test("Markdown Grammar changed H3", () => {
+   expect(markDowntoHTML("### test").toBe("<h3>test</h3>"));
+});
+test("Markdown Grammar changed H2", () => {
+   expect(markDowntoHTML("## test").toBe("<h2>test</h2>"));
+});
+test("Markdown Grammar changed H1", () => {
+   expect(markDowntoHTML("# test").toBe("<h1>test</h1>"));
 });
